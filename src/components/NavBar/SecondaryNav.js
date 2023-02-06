7e231c1f1312c1516b84383aaca8c708a7fd5c10
Jeris Rue <jeris.rue@gmail.com>
2023-02-05 22:11:38 -0600

    removed open-calls now that they are all done

--- src/components/NavBar/SecondaryNav.js
+++ src/components/NavBar/SecondaryNav.js
@@ -22,7 +22,7 @@ export default class SecondaryNav extends Component {
 								<a href="/agenda">Agenda</a>
 								<a href="/venue">Venue</a>
 								<a href="/dates">Dates</a>
-								<a href="/open-calls">Open Calls</a>
+								{/* <a href="/open-calls">Open Calls</a> */}
 								{/* <a href="/cfp">CFP</a> */}
                 				<a href="/training">Training</a>
 								<a href="/register">Register</a>
