aab297d98f8a6f37835649927ca4fd04c1b22365
Jeris <jrue3084@gmail.com>
2023-02-08 16:44:11 -0600

    added mastodon to oc page

--- src/pages/About/Organizers.js
+++ src/pages/About/Organizers.js
@@ -41,7 +41,8 @@ class Organizers extends Component {
               <div className='oc-text-section'>
                 <div className='oc-name'>{el.name}</div>
                 <div className='oc-title'>{el.title}</div>
-                {el.twitter && <a className='oc-twitter' href={`https://twitter.com/${el.twitter}`}>{`@${el.twitter}`}</a>}
+                {el.twitter && <a className='oc-twitter' target='_blank' rel='noopener noreferrer' href={`https://twitter.com/${el.twitter}`}>{`@${el.twitter}`}</a>}
+                {el.mastodon && <a className='oc-twitter' target='_blank' rel='noopener noreferrer' href={`${el.mastodon_url}`}>{`@${el.mastodon}`}</a>}
               </div>
             </div>
           </div>
